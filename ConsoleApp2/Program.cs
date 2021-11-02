ed13396e80c026507a1b9d1426897d15505bc520
Marek Valenta <Valenta.Marek@stud.vassboskovice.cz>
2021-11-02 11:26:22 +0100

    Kruh

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,4 @@
-﻿&using System;
+﻿using System;
 
 namespace ConsoleApp2
 {
@@ -9,8 +9,43 @@ namespace ConsoleApp2
             
             {
                 double polomer;
+                double obvod;
+                double obsah;
                 double prumer;
-                Console.WriteLine()
+                double vyber;
+                
+                Console.WriteLine("Obvod kruhu :");
+                Console.ReadLine();
+                Console.WriteLine("Vyber si z možností : 1 - Poloměr   2 - Průměr");
+
+                double.TryParse(Console.ReadLine(), out vyber);
+
+                if (vyber == 1)
+                {
+                    Console.WriteLine("Zadej poloměr");
+                    double.TryParse(Console.ReadLine(), out polomer);
+
+                    obvod = 2 * 3.14 * polomer;
+                    Console.WriteLine($"Obvod kruhu je : {obvod}");
+                    Console.ReadLine();
+                    obsah = 3.14 * (polomer * polomer);
+                    Console.WriteLine($"Obsah kruhu je : {obsah}");
+
+                }
+                else if (vyber == 1) ;
+                {
+                    Console.WriteLine("Zadej průměr");
+                    double.TryParse(Console.ReadLine(), out prumer);
+
+                    obvod = 3.14 * prumer;
+                    Console.WriteLine($"Obvod kruhu je : {obvod}");
+                    Console.ReadLine();
+                    obsah = 3.14 * (prumer * prumer) / 4;
+                    Console.WriteLine($"Obsah kruhu je : {obsah} ");
+
+
+                }
+                
             }
         }
     }

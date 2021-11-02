95401e653447ebbf9ab8c07d2ca949e0b65e7fd7
Marek Valenta <Valenta.Marek@stud.vassboskovice.cz>
2021-11-02 10:18:12 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿&using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                double polomer;
+                double prumer;
+                Console.WriteLine()
+            }
+        }
+    }
+}
